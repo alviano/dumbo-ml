75bf3d705624b4e98316d1d01c49c6e4f40b9887
Mario Alviano <mario.alviano@gmail.com>
2024-03-14 10:07:10 +0100

    finally free gpu

--- dumbo_ml/utils.py
+++ dumbo_ml/utils.py
@@ -16,36 +16,37 @@ def run_in_any_gpu(smi):
     def decorator(fun):
         @functools.wraps(fun)
         def wrapper(*args, **kwargs):
-            smi.smi_initialize()
-            devices = [device for device in range(smi.smi_get_device_count())]
-            random.shuffle(devices)
-            log.info(f"Try with order {devices}")
+            try:
+                smi.smi_initialize()
+                devices = [device for device in range(smi.smi_get_device_count())]
+                random.shuffle(devices)
+                log.info(f"Try with order {devices}")
 
-            delay = 1.0
-            done = False
-            while not done:
-                try:
-                    for device in devices:
-                        utilization = smi.smi_get_device_utilization(device)
-                        memory = 100 * smi.smi_get_device_memory_used(device) // smi.smi_get_device_memory_total(device)
-                        usage = f"(utilization {utilization}%%, memory {memory}%%)"
-                        if utilization > 10 or memory > 10:
-                            log.info(f"Skip GPU {device} {usage}")
-                            continue
-                        with tf.Graph().as_default():
-                            with tf.device(f"/GPU:{device}"):
-                                log.info(f"Try with GPU {device} {usage}")
-                                result = fun(*args, **kwargs)
-                                done = True
-                                break
-                except tf.errors.OpError as e:
-                    log.error(f"Oops!", e)
-                if not done:
-                    log.info(f"Retry in {delay:.1f} seconds...")
-                    time.sleep(delay)
-                    delay = min(300, delay * 2)
-
-            smi.smi_shutdown()
+                delay = 1.0
+                done = False
+                while not done:
+                    try:
+                        for device in devices:
+                            utilization = smi.smi_get_device_utilization(device)
+                            memory = 100 * smi.smi_get_device_memory_used(device) // smi.smi_get_device_memory_total(device)
+                            usage = f"(utilization {utilization}%%, memory {memory}%%)"
+                            if utilization > 10 or memory > 10:
+                                log.info(f"Skip GPU {device} {usage}")
+                                continue
+                            with tf.Graph().as_default():
+                                with tf.device(f"/GPU:{device}"):
+                                    log.info(f"Try with GPU {device} {usage}")
+                                    result = fun(*args, **kwargs)
+                                    done = True
+                                    break
+                    except tf.errors.OpError as e:
+                        log.error(f"Oops!", e)
+                    if not done:
+                        log.info(f"Retry in {delay:.1f} seconds...")
+                        time.sleep(delay)
+                        delay = min(300, delay * 2)
+            finally:
+                smi.smi_shutdown()
             return result
         return wrapper
     return decorator
